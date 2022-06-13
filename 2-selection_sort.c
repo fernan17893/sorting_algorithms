4b52fddd08362a779f76e22a925d20bde35036f6
hoodrich-fern <fernan17893@gmail.com>
2022-06-13 17:15:19 +0000

    task 3

---
+++ 2-selection_sort.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+
+/**
+* selection_short- sorts an array of integers in ascending order
+* using Selection sort algorithm.
+* @array- array of integers
+* @size: size of array
+*
+*/
+
+void selection_sort(int *array, size_t size)
+{
+
+	int val = 0, indx = 0, flag = 0;
+	size_t i, j;
+
+	if (array == NULL)
+		return;
+	for (i = 0; i < size - 1; i++)
+	{
+		val = array[i];
+		for (j = i; j < size; j++)
+		{
+			if (val > array[j])
+			{
+				indx = j;
+				val = array[j];
+				flag = 1;
+			}
+		}
+		if (flag == 1)
+		{
+			array[indx] = array[i];
+			array[i] = val;
+			print_array(array, size);
+		}
+		flag = 0;
+	}
+}
