f0c757756a40089f775ca3174a9fa8307e635737
hoodrich-fern <fernan17893@gmail.com>
2022-06-15 02:35:52 +0000

    output error variable fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -17,9 +17,9 @@ void bubble_sort(int *array, size_t size)
 
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size - i - 1; j++)
+		for (j = 0; j < size - 1 - i; j++)
 		{
-			if (array[i] > array[j + 1])
+			if (array[j] > array[j + 1])
 			{
 				tmp = array[j + 1];
 				array[j + 1] = array[j];
